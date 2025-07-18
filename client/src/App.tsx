2e9d817129d3a5d89558324b0e363fbad6e82177
fares yared <sheinfw3@gmail.com>
2025-07-18 04:03:06 +0300

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <h1 className="text-4xl font-bold text-center pt-10 text-red-500">Reelify</h1>
+    </div>
+  )
+}
+
+export default App
